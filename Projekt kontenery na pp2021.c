dbc715010f4fd379a74f80abfd854f2eb03c0d7c
Asia <Asia@DESKTOP-FIMHAT2>
2021-12-26 00:42:54 +0100

    zmiany void spis

--- Projekt kontenery na pp2021.c
+++ Projekt kontenery na pp2021.c
@@ -5,27 +5,40 @@
 #include "listy.h"
 
 
-void spis(FILE* plikfunc, ADRES pierwsza)     //funkcja spisujaca liste z pliku
+void spis(FILE* plikfunc, ADRES pierwsza, lista **adres1, lista* nowy)     //funkcja spisujaca liste z pliku
 {
 	ADRES pom = pierwsza;
+	if (pom==NULL) pom = pom->next;
 	while (pom)
 	{
-		fscanf(plikfunc, "%d", pom->d);
 		pom = pom->next;
 	}
+
+	
+	while (*adres1 != NULL) adres1 = &((*adres1)->next);
+	*adres1 = nowy;
+	nowy->next = NULL;
+	}
+	
+	
+	
+	
+
 }
 
 int main()
 {
-	ADRES pierwszy = NULL, ostatni = NULL, pom=NULL;
+	ADRES pierwszy = NULL, ostatni = NULL, pom = NULL;
 	pom = (ADRES)malloc(sizeof(LISTA));
 
 	int obj;
 
-	FILE * plik = fopen("kontenery.txt", "r");
-	printf("Wpisz objetosc metryczna ladunku: "); 
+	FILE* plik = fopen("kontenery.txt", "r");
 
+	printf("Wpisz objetosc metryczna ladunku: ");
+	if(scanf("%d", &obj)<=0)printf("nieprawidlowa objetosc");
 	printf("\n");
+
 	spis(plik, pierwszy);
 	printf("%d %d %d %d", pom->d, pom->s, pom->h, pom->yop);
 }
--- listy.h
+++ listy.h
@@ -1,11 +1,10 @@
 #pragma once
 
-struct lista
+typedef struct lista
 {
 	int d, s, h, yop, v; // d - szerokosc, s - dlugosc, h - glebokosc, yop - rok produkcji, v - wartosc
-	int* d, * s, * h, * yop, * v;
 	struct lista* next;
-};
+} lista ;
 
 typedef struct lista LISTA;
 typedef LISTA* ADRES;
