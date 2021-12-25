9376ffa73137a014e2d332de1d1967f7fb6d0f97
kubutek02 <kubutek02@DESKTOP-4JIEB5R>
2021-12-25 10:53:09 +0100

    aktualizacja

--- Projekt kontenery na pp2021.c
+++ Projekt kontenery na pp2021.c
@@ -1,22 +1,32 @@
-﻿// Projekt kontenery na pp2021.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿#pragma warning(disable : 4996)
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include "listy.h"
 
 
-
+void spis(FILE* plikfunc, ADRES pierwsza)     //funkcja spisujaca liste z pliku
+{
+	ADRES pom = pierwsza;
+	while (pom)
+	{
+		fscanf(plikfunc , "%d %d %d %d" , pom->d, pom->s,pom->h,pom->yop);
+		pom = pom->next;
+	}
+}
 
 int main()
 {
 	ADRES pierwszy = NULL, ostatni = NULL, pom;
+	pom = (ADRES)malloc(sizeof(LISTA));
 	int obj;
-	FILE* plik = fopen("kontenery.txt", "rw");
+
+	FILE* plik = fopen("kontenery.txt", "r");
 	printf("Wpisz objetosc metryczna ladunku: "); 
-	scanf("%d", &obj);
+
+	printf("\n");
 	spis(plik,pierwszy);
+	printf("%d %d %d %d", pom->d, pom->s, pom->h, pom->yop);
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
--- listy.h
+++ listy.h
@@ -6,15 +6,5 @@ struct lista
 	struct lista* next;
 };
 
-void spis(FILE *plikfunc, ADRES pierwszy)     //funkcja spisujaca liste z pliku
-{
-    ADRES pom = pierwszy;
-    while (pom)
-    {
-        fscanf(plikfunc, pom->d, pom->s, pom->h, pom->yop, pom->v);
-        pom = pom->next;
-    }
-}
-
 typedef struct lista LISTA;
 typedef LISTA* ADRES;
