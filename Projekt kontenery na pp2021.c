a04b6e29f4b0958cf12f68b2f91ba8a977473e00
kubutek02 <kubutek02@DESKTOP-4JIEB5R>
2021-12-30 15:31:08 +0100

    poprawka z Lewym

--- Projekt kontenery na pp2021.c
+++ Projekt kontenery na pp2021.c
@@ -1,4 +1,5 @@
 ﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6011)
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -17,86 +18,98 @@ int liczba_linii(FILE* plikfunc)   //zwraca liczbe linii w pliku
 		}
 	}
 	return lines+1;
+
 }
 
-void spis(FILE* plikfunc, ADRES pierwszafunc,ADRES ostatnifunc, ADRES pomfunc)     //funkcja spisujaca liste z pliku
+void spis(int tab[], t_element ** adpocz,int liczba_linii_func)     //funkcja spisujaca liste z pliku, polaczyc z sortowaniem
 {
-	ADRES pom = pierwszafunc;
-	if (pom == NULL) 
+	t_element* temp;
+	*adpocz = NULL;
+	for (int i = 0; i < liczba_linii_func; i++)
 	{
-		pierwszafunc = pom;
-		ostatnifunc = pom;
-	}
-	else
-	{
-		ostatnifunc->next;
-		ostatnifunc = pom;
-	}
-
-	while (pom)
-	{
-		fscanf(plikfunc, "%d", &pom->d);
-		fscanf(plikfunc, "%d", &pom->s);
-		fscanf(plikfunc, "%d", &pom->h);
-		fscanf(plikfunc, "%d", &pom->yop);
-		pom = pom->next;
+		temp = (t_element*)malloc(sizeof(t_element));
+		temp->d = tab[i * 5];
+		temp->s = tab[i * 5 + 1];
+		temp->h = tab[i * 5 + 2];
+		temp->yop = tab[i * 5 + 3];
+		temp->v = tab[i * 5 + 4];
+		temp->obj = ((temp->d)/100) * ((temp->s)/100) * ((temp->h)/100);
+		temp->next = *adpocz;
+		*adpocz = temp;
 	}
 }
 
-void WypiszListe(ADRES pierwszy)
+void sort_listy(int tab[],t_element *adpocz,int liczba_linii_func)
 {
-	ADRES pom = pierwszy;
-	while (pom)
+	t_element* temp;
+	t_element* e, * p;
+	int v;
+
+
+	temp = (t_element*)malloc(sizeof(t_element));
+	temp->next = (t_element*)malloc(sizeof(t_element));
+	temp->next->next = NULL;
+	temp->next->obj = 2147483647;
+
+	for (int i = 0; i < liczba_linii_func; i++)
 	{
-		printf("%d %d %d %d", pom->d, pom->s, pom->h, pom->yop);
-		pom = pom->next;
-	}
 
-	
-	while (*adres1 != NULL) adres1 = &((*adres1)->next);
-	*adres1 = nowy;
-	nowy->next = NULL;
-	}
-	
-	
-	
-	
 
+		v = adpocz->obj;
+
+		for (p = temp; v > p->next->obj; p = p->next);
+		
+		e = (t_element*)malloc(sizeof(t_element));
+		e->obj = v;
+		e->next = p->next;
+		p->next = e;
+		adpocz = adpocz->next;
+	}
+	for (p = temp->next; p->next; p = p->next) printf("%d ", p->obj);
+	printf("\n");
+}
+
+void WypiszListe(t_element * poczatek)
+{
+	while (poczatek)
+	{
+		printf("%d ", poczatek->d);
+		printf("%d ", poczatek->s);
+		printf("%d ", poczatek->h);
+		printf("%d ", poczatek->yop);
+		printf("%d ", poczatek->v);
+		printf("%d \n", poczatek->obj);
+		poczatek = poczatek->next;
+	}
 }
 
 int main()
 {
-	//ADRES pierwszy = NULL, ostatni = NULL, pom;
-	//pom = (ADRES)malloc(sizeof(LISTA));
-	FILE *plik = fopen("kontenery.txt", "r");
+
+	FILE* plik;
+	plik = fopen("abc.txt", "r");
 	if (!plik) { printf("Plik nie mogl zostac otwarty."); return 1; }
 	int w = liczba_linii(plik);
-	int k = 5;
+	fclose(plik);
 
-	int** tab = (int**)malloc(w * sizeof(int*));
-	for (int i = 0; i < w; i++)
+	plik = fopen("abc.txt", "r");
+	if (!plik) { printf("Plik nie mogl zostac otwarty."); return 1; }
+
+	int* tab = NULL;
+	tab = (int*)malloc(w * 5 * sizeof(int));
+	for (int i = 0; i < w*5;)
 	{
-		tab[i] = (int*)malloc(k * sizeof(int));      ///   C6386
+		fscanf(plik, "%d %d %d %d %d", &tab[i], &tab[i + 1], &tab[i + 2], &tab[i + 3], &tab[i + 4]);
+		i += 5;
 	}
-
-	for (int i = 0; i < w; i++)
+	for (int i = 0; i < w*5;)
 	{
-		fscanf(plik, "%d %d %d %d %d", &tab[i][0], &tab[i][1], &tab[i][2], &tab[i][3], &tab[i][4]);     ///     C6385
+		printf("%d %d %d %d %d\n", tab[i], tab[i + 1], tab[i + 2], tab[i + 3], tab[i + 4]);
+		i += 5;
 	}
-
-	for (int i = 0; i < w; i++)
-	{
-		printf(plik, "%d %d %d %d %d", tab[i][0], tab[i][1], tab[i][2], tab[i][3], tab[i][4]);     ///wyjątek podczas kompilacji
-	}
-	//printf("Wpisz objetosc metryczna ladunku: ");
-
-	//scanf("%d", &obj);
-	//printf("\n");
-		
-	//spis(plik, pierwszy,ostatni,pom);
-	//printf("%d %d %d %d", pom->d, pom->s, pom->h, pom->yop); 
-
-
-
-
+	t_element* poczatek;
+	spis(tab, &poczatek, w);
+	WypiszListe(poczatek);
+	sort_listy(tab,poczatek, w);
+	printf("\n");
 }
--- listy.h
+++ listy.h
@@ -1,10 +1,10 @@
 #pragma once
 
-typedef struct lista
+typedef struct element
 {
-	int d, s, h, yop, v; // d - szerokosc, s - dlugosc, h - glebokosc, yop - rok produkcji, v - wartosc
-	struct lista* next;
-} lista ;
+	int d, s, h, yop, v, obj; // d - szerokosc, s - dlugosc, h - glebokosc, yop - rok produkcji, v - wartosc
+	struct element* next;
+} t_element;
 
-typedef struct lista LISTA;
-typedef LISTA* ADRES;
+//typedef struct lista LISTA;
+//typedef LISTA* ADRES;
